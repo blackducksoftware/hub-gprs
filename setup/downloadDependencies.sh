5db1b26f4c18044bcf06150a7c3f99d419bf5b5c
Dave Meurer <dmeurer@synopsys.com>
2019-08-19 17:57:57 -0400

    Upgrade to Synopsys Detect 5.5.1

--- setup/downloadDependencies.sh
+++ setup/downloadDependencies.sh
@@ -1,49 +1,45 @@
 #!/bin/bash
 
-export HUB_DETECT_VERSION=3.1.1
+export HUB_DETECT_VERSION=5.5.1
 export EXPECTED_JDK_MD5=9c02c89f37d217d229203e59b57246a8
 
 compareMd5Sum(){
-	file=$1
-	expected=$2
-	actual="$(cat ""${file}"" 2>/dev/null | md5)"
-	if [ "$expected" != "$actual" ]; then
-		echo 'bad'
-	fi
+			file=$1
+			expected=$2
+			actual="$(cat ""${file}"" 2>/dev/null | md5)"
+			if [ "$expected" != "$actual" ]; then
+							echo 'bad'
+			fi
 }
 
 if [ ! -d ./download ]; then
-    mkdir download
+	mkdir download
 fi;
 
 if [ -e ./download/jdk.tar.gz ] && [ "$(compareMd5Sum ./download/jdk.tar.gz $EXPECTED_JDK_MD5)" = "bad" ]; then
-	echo Incorrect prior JDK download detected $(cat ./download/jdk.tar.gz | md5). Re-downloading...
-	rm ./download/jdk.tar.gz
+			echo Incorrect prior JDK download detected $(cat ./download/jdk.tar.gz | md5). Re-downloading...
+			rm ./download/jdk.tar.gz
 fi
 
 #Download the JRE/JDK
 if [ ! -e ./download/*jdk*.tar.gz ]; then
-    wget https://cdn.azul.com/zulu/bin/zulu8.28.0.1-jdk8.0.163-linux_x64.tar.gz -O download/jdk.tar.gz
-    downloadSuccess=$?
-    if [ "${downloadSuccess}" -ne "0" ]; then
-        >&2 echo JDK Download failed.
-        rm -f download/jdk.tar.gz
-        exit 1
-    fi
+	wget https://cdn.azul.com/zulu/bin/zulu8.28.0.1-jdk8.0.163-linux_x64.tar.gz -O download/jdk.tar.gz
+	downloadSuccess=$?
+	if [ "${downloadSuccess}" -ne "0" ]; then
+			>&2 echo JDK Download failed.
+			rm -f download/jdk.tar.gz
+			exit 1
+	fi
 fi
 
-#Download hub-detect
-if [ ! -e ./download/hub-detect.sh ] || [ ! -e ./download/hub-detect-${HUB_DETECT_VERSION}.jar ]; then 
-    rm -f download/hub-detect*
-    wget "https://blackducksoftware.github.io/hub-detect/hub-detect.sh" -O download/hub-detect.sh && wget "https://test-repo.blackducksoftware.com/artifactory/bds-integrations-release/com/blackducksoftware/integration/hub-detect/${HUB_DETECT_VERSION}/hub-detect-${HUB_DETECT_VERSION}.jar" -O download/hub-detect-${HUB_DETECT_VERSION}.jar
-    downloadSuccess=$?
-    if [ "${downloadSuccess}" -ne "0" ]; then
-        >&2 echo Hub-Detect download failed.
-        rm -f download/hub-detect*
-        exit 1
-    fi  
+#Download synopsys-detect
+if [ ! -e ./download/synopsys-detect.sh ] || [ ! -e ./download/synopsys-detect-${HUB_DETECT_VERSION}.jar ]; then
+	rm -f download/synopsys-detect*
+	wget "https://detect.synopsys.com/detect.sh" -O download/synopsys-detect.sh && wget "https://repo.blackducksoftware.com/artifactory/bds-integrations-release/com/synopsys/integration/synopsys-detect/${HUB_DETECT_VERSION}/synopsys-detect-${HUB_DETECT_VERSION}.jar" -O download/synopsys-detect-${HUB_DETECT_VERSION}.jar
+	downloadSuccess=$?
+	if [ "${downloadSuccess}" -ne "0" ]; then
+			>&2 echo Synopsys-Detect download failed.
+			rm -f download/synopsys-detect*
+			exit 1
+	fi
 fi
-
-
-
-
